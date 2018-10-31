61918576c3e44a79944fb4af3bc59f840671f1e6
Corey Hanson <hanson.cor@gmail.com>
2018-10-31 09:03:36 -0400

    Expirimenting

--- scripts/browserstack_prod.sh
+++ scripts/browserstack_prod.sh
@@ -1,26 +1,29 @@
+#!/bin/bash
+
 err=0
 trap 'err=1' ERR
 
-node_modules/.bin/testcafe 'browserstack:chrome:Windows 10' test/testcafe/regression-tests.js
-node_modules/.bin/testcafe 'browserstack:firefox:Windows 10' test/testcafe/regression-tests.js
-node_modules/.bin/testcafe 'browserstack:edge:Windows 10' test/testcafe/regression-tests.js
-node_modules/.bin/testcafe 'browserstack:safari:Windows 10' test/testcafe/regression-tests.js
+echo node_modules/.bin/testcafe 'browserstack:chrome:Windows 10' test/testcafe/regression-tests.js;
+# node_modules/.bin/testcafe 'browserstack:firefox:Windows 10' test/testcafe/regression-tests.js
+# node_modules/.bin/testcafe 'browserstack:edge:Windows 10' test/testcafe/regression-tests.js
+# node_modules/.bin/testcafe 'browserstack:safari:Windows 10' test/testcafe/regression-tests.js
 
-node_modules/.bin/testcafe 'browserstack:chrome:Windows 8.1' test/testcafe/regression-tests.js
-node_modules/.bin/testcafe 'browserstack:firefox:Windows 8.1' test/testcafe/regression-tests.js
-node_modules/.bin/testcafe 'browserstack:ie@11.0:Windows 8.1' test/testcafe/regression-tests.js
+echo node_modules/.bin/testcafe 'browserstack:chrome:Windows 8.1' test/testcafe/regression-tests.js;
+echo node_modules/.bin/testcafe 'browserstack:firefox:Windows 8.1' test/testcafe/regression-tests.js;
+echo node_modules/.bin/testcafe 'browserstack:ie@11.0:Windows 8.1' test/testcafe/regression-tests.js;
+node_modules/.bin/testcafe 'browserstack:ie@11.0:Windows 8.1' test/testcafe/regression-tests.js;
 
-node_modules/.bin/testcafe 'browserstack:chrome:Windows 8' test/testcafe/regression-tests.js
-node_modules/.bin/testcafe 'browserstack:firefox:Windows 8' test/testcafe/regression-tests.js
-
-node_modules/.bin/testcafe 'browserstack:chrome:OS X High Sierra' test/testcafe/regression-tests.js
-node_modules/.bin/testcafe 'browserstack:firefox:OS X High Sierra' test/testcafe/regression-tests.js
-node_modules/.bin/testcafe 'browserstack:safari:OS X High Sierra' test/testcafe/regression-tests.js
-
-node_modules/.bin/testcafe 'browserstack:chrome:OS X Sierra' test/testcafe/regression-tests.js
-node_modules/.bin/testcafe 'browserstack:firefox:OS X Sierra' test/testcafe/regression-tests.js
-
-node_modules/.bin/testcafe 'browserstack:chrome:OS X El Capitan' test/testcafe/regression-tests.js
-node_modules/.bin/testcafe 'browserstack:firefox:OS X El Capitan' test/testcafe/regression-tests.js
+# node_modules/.bin/testcafe 'browserstack:chrome:Windows 8' test/testcafe/regression-tests.js
+# node_modules/.bin/testcafe 'browserstack:firefox:Windows 8' test/testcafe/regression-tests.js
+#
+# node_modules/.bin/testcafe 'browserstack:chrome:OS X High Sierra' test/testcafe/regression-tests.js
+# node_modules/.bin/testcafe 'browserstack:firefox:OS X High Sierra' test/testcafe/regression-tests.js
+# node_modules/.bin/testcafe 'browserstack:safari:OS X High Sierra' test/testcafe/regression-tests.js
+#
+# node_modules/.bin/testcafe 'browserstack:chrome:OS X Sierra' test/testcafe/regression-tests.js
+# node_modules/.bin/testcafe 'browserstack:firefox:OS X Sierra' test/testcafe/regression-tests.js
+#
+# node_modules/.bin/testcafe 'browserstack:chrome:OS X El Capitan' test/testcafe/regression-tests.js
+# node_modules/.bin/testcafe 'browserstack:firefox:OS X El Capitan' test/testcafe/regression-tests.js
 
 test $err = 0
