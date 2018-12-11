7210cac0d7a2aa1338657ab9085e94d67643a0e0
Will O'Brien <will.obrien@dibit.co>
2018-12-11 08:53:34 -0500

    Revert "Moar echo"
    This reverts commit 3927025e3778952b90334ac143e904269c517e30.

--- scripts/browserstack_beta.sh
+++ scripts/browserstack_beta.sh
@@ -3,19 +3,14 @@
 err=0
 trap 'err=1' ERR
 
-node_modules/.bin/testcafe 'browserstack:chrome:Windows 10' test/testcafe/regression-tests.js --beta;
-echo $?;
-node_modules/.bin/testcafe 'browserstack:firefox:Windows 10' test/testcafe/regression-tests.js --beta;
-echo $?;
-node_modules/.bin/testcafe 'browserstack:edge:Windows 10' test/testcafe/regression-tests.js --beta;
-echo $?;
+echo $(node_modules/.bin/testcafe 'browserstack:chrome:Windows 10' test/testcafe/regression-tests.js --beta);
+echo $(node_modules/.bin/testcafe 'browserstack:firefox:Windows 10' test/testcafe/regression-tests.js --beta);
+echo $(node_modules/.bin/testcafe 'browserstack:edge:Windows 10' test/testcafe/regression-tests.js --beta);
 # node_modules/.bin/testcafe 'browserstack:safari:Windows 10' test/testcafe/regression-tests.js --beta
 
-node_modules/.bin/testcafe 'browserstack:chrome:Windows 8.1' test/testcafe/regression-tests.js --beta;
-echo $?;
+echo $(node_modules/.bin/testcafe 'browserstack:chrome:Windows 8.1' test/testcafe/regression-tests.js --beta);
 # node_modules/.bin/testcafe 'browserstack:firefox:Windows 8.1' test/testcafe/regression-tests.js --beta
-node_modules/.bin/testcafe 'browserstack:ie@11.0:Windows 8.1' test/testcafe/regression-tests.js --beta;
-echo $?;
+echo $(node_modules/.bin/testcafe 'browserstack:ie@11.0:Windows 8.1' test/testcafe/regression-tests.js --beta);
 node_modules/.bin/testcafe 'browserstack:ie@11.0:Windows 8.1' test/testcafe/regression-tests.js --beta;
 
 # node_modules/.bin/testcafe 'browserstack:chrome:Windows 8' test/testcafe/regression-tests.js --beta
