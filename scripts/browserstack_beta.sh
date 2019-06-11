61d408be53b6028bb6c2cc5e8c28e59c7aabf019
Anton de Winter <adewinter@gmail.com>
2019-06-11 17:16:25 -0600

    more script updates

--- scripts/browserstack_beta.sh
+++ scripts/browserstack_beta.sh
@@ -3,25 +3,8 @@
 err=0
 trap 'err=1' ERR
 
-node_modules/.bin/testcafe 'browserstack:chrome:Windows 10' test/testcafe/regression-tests.js --beta;
-node_modules/.bin/testcafe 'browserstack:firefox:Windows 10' test/testcafe/regression-tests.js --beta;
-node_modules/.bin/testcafe 'browserstack:edge@17.0:Windows 10' test/testcafe/regression-tests.js --beta;
-
-node_modules/.bin/testcafe 'browserstack:chrome:Windows 8.1' test/testcafe/regression-tests.js --beta;
-node_modules/.bin/testcafe 'browserstack:firefox:Windows 8.1' test/testcafe/regression-tests.js --beta
-node_modules/.bin/testcafe 'browserstack:ie@11.0:Windows 8.1' test/testcafe/shits-not-on-fire-tests.js --beta;
-
-node_modules/.bin/testcafe 'browserstack:chrome:Windows 8' test/testcafe/regression-tests.js --beta
-node_modules/.bin/testcafe 'browserstack:firefox:Windows 8' test/testcafe/regression-tests.js --beta
-
-node_modules/.bin/testcafe 'browserstack:chrome:OS X High Sierra' test/testcafe/regression-tests.js --beta
-node_modules/.bin/testcafe 'browserstack:firefox:OS X High Sierra' test/testcafe/regression-tests.js --beta
+node_modules/.bin/testcafe 'browserstack:chrome:Windows 10,browserstack:firefox:Windows 10' test/testcafe/regression-tests.js --beta
+node_modules/.bin/testcafe 'browserstack:edge@17.0:Windows 10,browserstack:chrome:OS X High Sierra' test/testcafe/regression-tests.js --beta
 node_modules/.bin/testcafe 'browserstack:safari:OS X High Sierra' test/testcafe/regression-tests.js --beta
-
-node_modules/.bin/testcafe 'browserstack:chrome:OS X Sierra' test/testcafe/regression-tests.js --beta
-node_modules/.bin/testcafe 'browserstack:firefox:OS X Sierra' test/testcafe/regression-tests.js --beta
-
-node_modules/.bin/testcafe 'browserstack:chrome:OS X El Capitan' test/testcafe/regression-tests.js --beta
-node_modules/.bin/testcafe 'browserstack:firefox:OS X El Capitan' test/testcafe/regression-tests.js --beta
 
 test $err = 0
