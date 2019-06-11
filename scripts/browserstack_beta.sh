aa1228177840f569eecee802b5543e4ee6d9d913
Anton de Winter <adewinter@gmail.com>
2019-06-11 16:48:44 -0600

    Test tweak to browserstack to run in parallel

--- scripts/browserstack_beta.sh
+++ scripts/browserstack_beta.sh
@@ -3,11 +3,8 @@
 err=0
 trap 'err=1' ERR
 
-node_modules/.bin/testcafe 'browserstack:chrome:Windows 10' test/testcafe/regression-tests.js --beta;
-node_modules/.bin/testcafe 'browserstack:firefox:Windows 10' test/testcafe/regression-tests.js --beta;
-node_modules/.bin/testcafe 'browserstack:edge@17.0:Windows 10' test/testcafe/regression-tests.js --beta;
-
-node_modules/.bin/testcafe 'browserstack:chrome:OS X High Sierra' test/testcafe/regression-tests.js --beta
+node_modules/.bin/testcafe 'browserstack:chrome:Windows 10,browserstack:firefox:Windows 10' test/testcafe/regression-tests.js --beta
+node_modules/.bin/testcafe 'browserstack:edge@17.0:Windows 10,browserstack:chrome:OS X High Sierra' test/testcafe/regression-tests.js --beta
 node_modules/.bin/testcafe 'browserstack:safari:OS X High Sierra' test/testcafe/regression-tests.js --beta
 
 test $err = 0
