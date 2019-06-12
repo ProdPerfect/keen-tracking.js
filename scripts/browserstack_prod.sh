dba123ebc9308548da3987c0b93da1a88d3d65e8
Anton de Winter <adewinter@gmail.com>
2019-06-12 10:06:20 -0600

    Revert "more script updates"
    This reverts commit 61d408be53b6028bb6c2cc5e8c28e59c7aabf019.

--- scripts/browserstack_prod.sh
+++ scripts/browserstack_prod.sh
@@ -3,20 +3,25 @@
 err=0
 trap 'err=1' ERR
 
-node_modules/.bin/testcafe 'browserstack:chrome:Windows 10,browserstack:firefox:Windows 10' test/testcafe/regression-tests.js
+node_modules/.bin/testcafe 'browserstack:chrome:Windows 10' test/testcafe/regression-tests.js;
+node_modules/.bin/testcafe 'browserstack:firefox:Windows 10' test/testcafe/regression-tests.js
+node_modules/.bin/testcafe 'browserstack:edge@17.0:Windows 10' test/testcafe/regression-tests.js
 
-node_modules/.bin/testcafe 'browserstack:edge@17.0:Windows 10,browserstack:chrome:Windows 8.1' test/testcafe/regression-tests.js
+node_modules/.bin/testcafe 'browserstack:chrome:Windows 8.1' test/testcafe/regression-tests.js;
+node_modules/.bin/testcafe 'browserstack:firefox:Windows 8.1' test/testcafe/regression-tests.js;
+node_modules/.bin/testcafe 'browserstack:ie@11.0:Windows 8.1' test/testcafe/shits-not-on-fire-tests.js;
 
+node_modules/.bin/testcafe 'browserstack:chrome:Windows 8' test/testcafe/regression-tests.js
+node_modules/.bin/testcafe 'browserstack:firefox:Windows 8' test/testcafe/regression-tests.js
 
-node_modules/.bin/testcafe 'browserstack:firefox:Windows 8.1,browserstack:ie@11.0:Windows 8.1' test/testcafe/regression-tests.js
+node_modules/.bin/testcafe 'browserstack:chrome:OS X High Sierra' test/testcafe/regression-tests.js
+node_modules/.bin/testcafe 'browserstack:firefox:OS X High Sierra' test/testcafe/regression-tests.js
+node_modules/.bin/testcafe 'browserstack:safari:OS X High Sierra' test/testcafe/regression-tests.js
 
-node_modules/.bin/testcafe 'browserstack:chrome:Windows 8,browserstack:firefox:Windows 8' test/testcafe/regression-tests.js
+node_modules/.bin/testcafe 'browserstack:chrome:OS X Sierra' test/testcafe/regression-tests.js
+node_modules/.bin/testcafe 'browserstack:firefox:OS X Sierra' test/testcafe/regression-tests.js
 
-node_modules/.bin/testcafe 'browserstack:chrome:OS X High Sierra,browserstack:firefox:OS X High Sierra' test/testcafe/regression-tests.js
-node_modules/.bin/testcafe 'browserstack:safari:OS X High Sierra,browserstack:chrome:OS X Sierra' test/testcafe/regression-tests.js
-
-node_modules/.bin/testcafe 'browserstack:firefox:OS X Sierra,browserstack:chrome:OS X El Capitan' test/testcafe/regression-tests.js
-
+node_modules/.bin/testcafe 'browserstack:chrome:OS X El Capitan' test/testcafe/regression-tests.js
 node_modules/.bin/testcafe 'browserstack:firefox:OS X El Capitan' test/testcafe/regression-tests.js
 
 test $err = 0
